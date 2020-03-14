83ce774ea0cc57dfbdf4e8b286fe0fce7d5ff1a9
fagner henrique flores <henriquefagner@hotmail.com>
2020-03-14 16:34:25 -0500

    not done yet

---
+++ checkatrivalues/checkatrivalues.js
@@ -0,0 +1,11 @@
+var firstItem = document.getElementById("one"); //get first list item
+
+if (firstItem.hasAttribute("class")) {
+  //if it has class attribute
+
+  var attr = firstItem.getAttribute("class"); //get the attribute
+
+  var el = document.getElementById("scriptResults"); //add the value of the attribute after the list
+
+  el.innerHTML = "<p>the first item has a class name: " + attr + "</p>";
+}
